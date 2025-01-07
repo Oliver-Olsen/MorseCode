4af10e596ca7a260c330f0984aaa51f8cc300a9b
Metafysical <wulff43211234@gmail.com>
2025-01-07 11:46:48 +0100

    Created the loop function, and added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,44 @@
 #include <Arduino.h>
-#define LEDPIN 13
-#define TIMEUNIT 500
+
+#define LEDPIN 13 //Defines the pin used for the LED as pin 13
+#define TIMEUNIT 500 //Defines the amount if milliseconds in a time unit
+#define INPUT "SOS" //Defines the input we want to write with morse
+
+unsigned int wordLength; //Creates an unsigned integer, word
+
+char in[] = INPUT; //Creates a character array for each letter in the input
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200); 
-  pinMode(LEDPIN, OUTPUT); 
-  digitalWrite(LEDPIN, LOW); 
+  pinMode(LEDPIN, OUTPUT); //Sets LEDPIN as an output
+  digitalWrite(LEDPIN, LOW); //Ensures LEDPIN starts low
+
+  wordLength = strlen(INPUT); //Determines how many characters are in the input
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  for (int i = 0; i < wordLength; i++) //Runs a for loop for every character in the input
+    char letter = in[i]; //Assigns the current letter to a character 'letter'
+    morseConverter(letter); //Runs the current letter through the morse converter
 }
 
-void dot() {
-  digitalWrite(LEDPIN, HIGH); 
-  delay(1 * TIMEUNIT); 
-  digitalWrite(LEDPIN, LOW); 
-  delay(1 * TIMEUNIT); 
+void dot() { //A function which produces a dot with the LED
+  digitalWrite(LEDPIN, HIGH); //Turns on the LED
+  delay(1 * TIMEUNIT); //Waits for one time unit before turning off the LED
+  digitalWrite(LEDPIN, LOW); //Turns off the LED
+  delay(1 * TIMEUNIT); //Waits for a time unit before the next part of the letter
 }
 
-void dash() {
-  digitalWrite(LEDPIN, HIGH); 
-  delay(3 * TIMEUNIT); 
-  digitalWrite(LEDPIN, LOW); 
-  delay(1 * TIMEUNIT); 
+void dash() { //A function which produces a dash with the LED
+  digitalWrite(LEDPIN, HIGH); //Turns on the LED 
+  delay(3 * TIMEUNIT); //Waits for three time unit before turning off the LED
+  digitalWrite(LEDPIN, LOW); //Turns off the LED
+  delay(1 * TIMEUNIT); //Waits for a time unit before the next part of the letter
 }
 
 void newLetter() {
-  delay(3 * TIMEUNIT); 
+  delay(3 * TIMEUNIT); //Waits for three time units before the next letter begins
 }
 
 void newWord() {
-  delay(7 * TIMEUNIT); 
+  delay(1 * TIMEUNIT); //Waits for 1 time units before the next word begins, since newLetter will be triggered before and after this function, resulting in 7 time units tital
 }
