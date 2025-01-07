975687899e56c44404d8563cde987abd32285f05
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-07 11:00:11 +0100

    LEDPIN added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
-
+#define LEDPIN 13
 // put function declarations here:
-int myFunction(int, int);
+int myFunction(char);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -13,6 +13,6 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int myFunction(char word) {
+  return word;
 }
